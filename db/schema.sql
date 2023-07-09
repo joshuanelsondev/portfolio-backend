62ad0839dc619786124883d275fc5b91f3ff8adc
joshuanelsondev <joshuanelsondev@gmail.com>
2023-07-08 22:51:06 -0400

    Update and create dbConfig.js, schema.sql, seed.sql

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS portfolio;
+CREATE DATABASE portfolio;
+
+\c portfolio;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE projects (
+ id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+ title VARCHAR(50) NOT NULL,
+ description TEXT NOT NULL,
+ tech TEXT[],
+ image VARCHAR(200),
+ github_link VARCHAR(200),
+ demo_link VARCHAR(200),
+ is_favorite BOOLEAN,
+ created_on DATE,
+ updated_on DATE 
+);
